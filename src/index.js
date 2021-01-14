debeb4414e444081ad4f9cd90930e885b07a860d
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-14 08:56:14 -0600

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,52 +1,43 @@
-import loadMainPage from './main-page'
-import loadMenuPage from './menu-page'
-import loadContactPage from './contact-page'
+import loadMainPage from "./main-page";
+import loadMenuPage from "./menu-page";
+import loadContactPage from "./contact-page";
 
-let contentDiv = document.getElementById('content');
-let homeButton = document.getElementById('home-button');
-let menuButton = document.getElementById('menu-button');
-let contactButton = document.getElementById('contact-button');
+let contentDiv = document.getElementById("content");
+let homeButton = document.getElementById("home-button");
+let menuButton = document.getElementById("menu-button");
+let contactButton = document.getElementById("contact-button");
 
+function resetButton() {
+  homeButton = document.getElementById("home-button");
+  menuButton = document.getElementById("menu-button");
+  contactButton = document.getElementById("contact-button");
 
-function resetButton(){
-	homeButton = document.getElementById('home-button');
-	menuButton = document.getElementById('menu-button');
-	contactButton = document.getElementById('contact-button');
+  homeButton.addEventListener("click", function () {
+    loadMain();
+  });
 
-	homeButton.addEventListener('click', function(){
-		loadMain();
-	})
+  menuButton.addEventListener("click", function () {
+    loadMenu();
+  });
 
-	menuButton.addEventListener('click', function(){
-		loadMenu();
-	})
-
-	contactButton.addEventListener('click', function(){
-		loadContact();
-	})
-
-
-
+  contactButton.addEventListener("click", function () {
+    loadContact();
+  });
 }
 
-function loadMain(){
-	console.log("IM HEEERE")
-	contentDiv.innerHTML = loadMainPage();
-	resetButton();
+function loadMain() {
+  contentDiv.innerHTML = loadMainPage();
+  resetButton();
 }
 
-function loadMenu(){
-	contentDiv.innerHTML = loadMenuPage();
-	resetButton();
+function loadMenu() {
+  contentDiv.innerHTML = loadMenuPage();
+  resetButton();
 }
 
-function loadContact(){
-	contentDiv.innerHTML = loadContactPage();
-	resetButton();
+function loadContact() {
+  contentDiv.innerHTML = loadContactPage();
+  resetButton();
 }
-
 
 loadMain();
-
-
-
